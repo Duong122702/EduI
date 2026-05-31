0a90e8030af9753263029e8c56a0e483ad78f83c
duong_pc <nguyenhuyen241178@gmail.com>
2026-06-01 03:17:23 +0700

    chore(backend): write database.py

--- backend/src/app/core/config.py
+++ backend/src/app/core/config.py
@@ -6,10 +6,12 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
 
     # Chuỗi kết nối Database (Asyncpg cho PostgreSQL)
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:password@localhost:5432/app_db"
+    DATABASE_URL: str = "postgresql+asyncpg://postgres.immhnwnpiqgubdykmlvv:Yennhi16052007@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres"
 
     # Đọc từ file .env nếu có, ưu tiên biến môi trường hệ thống
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+
+    DB_ECHO: bool = True
 
 
 settings = Settings()
---
+++ backend/src/app/core/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from .config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=settings.DB_ECHO)
+
+# Create a session factorythat will be used to create new sessions for interacting with the database. (using async_sessionmaker to create asynchronous sessions)
+SessionLocal = async_sessionmaker(bind=engine, autoflush=False)
+
+# Base class for all the models in the application. It inherits from DeclarativeBase, which is a base class provided by SQLAlchemy for defining models using the declarative syntax.
+class Base(DeclarativeBase):
+    pass
+
+# Dependency function that can be used in FastAPI routes to get a database session. It uses the SessionLocal factory to create a new session and yields it. The session will be automatically closed after the request is completed.
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
