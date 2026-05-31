9023ed5d371a24d588448062ddf939c4980bfa2f
duong_pc <nguyenhuyen241178@gmail.com>
2026-06-01 03:36:43 +0700

    sec(backend): create env file and fix leak secret

--- backend/src/app/core/config.py
+++ backend/src/app/core/config.py
@@ -1,12 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+load_dotenv()  # Tải biến môi trường từ file .env
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Education Improvement API"
     API_V1_STR: str = "/api/v1"
 
     # Chuỗi kết nối Database (Asyncpg cho PostgreSQL)
-    DATABASE_URL: str = "postgresql+asyncpg://postgres.immhnwnpiqgubdykmlvv:Yennhi16052007@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
 
     # Đọc từ file .env nếu có, ưu tiên biến môi trường hệ thống
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
