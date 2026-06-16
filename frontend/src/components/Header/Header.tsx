f919aa03df17a763d5ff6bfeee8451506d9640c5
Duong Rakan <duong.cv.hec@gmail.com>
2026-06-16 19:37:47 +0700

    feat(home):create banner

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,8 +5,8 @@ import NavItem from './components/NavItem';
 function Header() {
   return (
     <>
-      <div className="fixed top-0 z-1000 w-full bg-white">
-        <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between border-b border-gray-200 px-4">
+      <div className="sticky top-0 z-1000 w-full border-b border-gray-100 bg-white/80 backdrop-blur-md">
+        <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between px-4">
           <Logo />
           <div className="flex gap-8 font-light opacity-70">
             <NavItem>Trang chủ</NavItem>
@@ -21,8 +21,26 @@ function Header() {
             </Button>
           </div>
         </div>
-        {/* Add pointer-events-none */}
-        <div className="z-0 border-b border-gray-200 before:pointer-events-none before:absolute before:bottom-0 before:left-0 before:h-full before:w-full before:content-['']"></div>
+      </div>
+      <div id="home" className="relative overflow-hidden pt-32 pb-32">
+        <div className="pointer-events-none absolute top-0 left-1/2 -z-10 h-full w-full max-w-7xl -translate-x-1/2 overflow-hidden">
+          <div className="absolute top-[10%] left-[-10%] h-125 w-125 rounded-full bg-teal-50/50 blur-3xl"></div>
+          <div className="absolute right-[-5%] bottom-[-10%] h-150 w-150 rounded-full bg-orange-50/30 blur-3xl"></div>
+        </div>
+        <div className="container mx-auto px-4 text-center">
+          <h1 className="mx-auto mb-6 max-w-4xl text-4xl font-bold text-gray-900 md:text-6xl">
+            Nền tảng Thi trực tuyến
+            <br className="hidden md:block" />
+            <span className="bg-linear-to-r from-teal-600 to-teal-400 bg-clip-text leading-snug text-transparent">
+              Khách quan & Bảo mật
+            </span>
+          </h1>
+          <p className="mx-auto mb-12 max-w-2xl text-xl leading-relaxed text-gray-500">
+            Tổ chức kỳ thi, làm bài kiểm tra và chấm điểm tự động dễ dàng trong
+            5 phút. Nhanh chóng, ổn định và hoàn toàn miễn phí.
+          </p>
+          <div className=""></div>
+        </div>
       </div>
     </>
   );
