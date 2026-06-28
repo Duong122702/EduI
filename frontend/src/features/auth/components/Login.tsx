07b626011ae4094415d8682c7f1f61990c0e7e5c
Duong Rakan <duong.cv.hec@gmail.com>
2026-06-28 22:35:43 +0700

    feat(auth): create input data login

--- frontend/src/features/auth/components/Login.tsx
+++ frontend/src/features/auth/components/Login.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 import Button from '../../../components/ui/Button';
 import { CustomIcon } from '../../../components/ui/CustomIcon';
 import { Input } from '../../../components/ui/Input';
 import type { LoginProps } from '../../../types/AuthTypes/login.type';
+import type { UserFormLoginValues } from '../schemas/login.schema';
 
 function Login({ currentTab }: LoginProps) {
+  const [loginFormData, setLoginFormData] = useState<UserFormLoginValues>({
+    email: '',
+    password: '',
+    acceptTerms: false,
+  });
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>
+  ) => {
+    const { name, value, type, checked } = e.target;
+    setLoginFormData((prev) => ({
+      ...prev,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    // 2. Chỉ gửi dữ liệu lên component cha khi user click Submit
+  };
   return (
     <div className={`${currentTab === 'login' ? 'block' : 'hidden'} mt-8`}>
       <div className="mb-5">
@@ -16,10 +36,13 @@ function Login({ currentTab }: LoginProps) {
             className="absolute top-1/2 left-4 -translate-y-1/2 text-gray-400"
           />
           <Input
+            name="email"
             type="email"
+            value={loginFormData.email}
             inputSize={'md'}
             className="bg-white px-10"
             placeholder="student@example.com"
+            onChange={handleInputChange}
           />
         </div>
       </div>
@@ -42,6 +65,9 @@ function Login({ currentTab }: LoginProps) {
           />
           <Input
             type="password"
+            name="password"
+            value={loginFormData.password}
+            onChange={handleInputChange}
             inputSize={'md'}
             className="items-center bg-white px-10"
             placeholder="••••••••"
@@ -55,7 +81,13 @@ function Login({ currentTab }: LoginProps) {
         </div>
       </div>
       <div className="mb-5 flex items-center justify-start gap-2">
-        <input type="checkbox" className="h-4 w-4" />
+        <input
+          type="checkbox"
+          className="h-4 w-4"
+          name="acceptTerms"
+          checked={loginFormData.acceptTerms}
+          onChange={handleInputChange}
+        />
         <span className="text-xs text-gray-800">
           Duy trì đăng nhập trong 30 ngày
         </span>
@@ -63,6 +95,7 @@ function Login({ currentTab }: LoginProps) {
       <Button
         variant={'dark'}
         className="w-full gap-2 rounded-2xl py-3 font-semibold shadow-xl"
+        onClick={handleSubmit}
       >
         Đăng nhập ngay
         <CustomIcon name="arrowRight" />
--- frontend/src/features/auth/schemas/login.schema.ts
+++ frontend/src/features/auth/schemas/login.schema.ts
@@ -1,12 +1,11 @@
 import * as Yup from 'yup';
 
-export const registerSchema = Yup.object().shape({
+export const loginSchema = Yup.object().shape({
   email: Yup.string().email('Email không hợp lệ').required('Email là bắt buộc'),
   password: Yup.string()
     .min(8, 'Mật khẩu phải từ 8 ký tự')
     .required('Mật khẩu là bắt buộc'),
-  acceptTerms: Yup.boolean().oneOf(
-    [true],
-    'Bạn phải đồng ý với điều khoản dịch vụ'
-  ),
+  acceptTerms: Yup.boolean().oneOf([true], 'Bạn đồng ý duy trì đăng nhập'),
 });
+
+export type UserFormLoginValues = Yup.InferType<typeof loginSchema>;
