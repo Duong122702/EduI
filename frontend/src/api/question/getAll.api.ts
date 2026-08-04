a59ba7c31001f15d821461ef1efba89730c3f56c
Duong Rakan <duong.cv.hec@gmail.com>
2026-08-05 02:01:15 +0700

    feat(questionbank): update call api getAll question

--- frontend/src/api/question/getAll.api.ts
+++ frontend/src/api/question/getAll.api.ts
@@ -1,18 +1,22 @@
 import type { ApiResponse } from '@/schemas/response/apiResponse';
 import axiosClient from '../config/axiosClient';
 import type { QuestionResponse } from '@/schemas/response/questionResponse';
+import type { GetQuestionsParams } from '@/schemas/payload/questionParamPayload.type';
 
-export const getAllQuestions = async (
-  page: number = 1,
-  page_size: number = 10
-) => {
+export const getAllQuestions = async (params: GetQuestionsParams) => {
+  const { page = 1, page_size = 10, ...filterParams } = params;
+
+  const rawParams = { page, page_size, ...filterParams };
+
+  const cleanParams = Object.fromEntries(
+    Object.entries(rawParams).filter(
+      ([_, value]) => value !== '' && value !== null && value !== undefined
+    )
+  );
   return await axiosClient.get<ApiResponse<QuestionResponse>>(
     '/questions/get_all',
     {
-      params: {
-        page,
-        page_size,
-      },
+      params: cleanParams,
     }
   );
 };
---
+++ frontend/src/schemas/payload/questionParamPayload.type.ts
@@ -0,0 +1,15 @@
+export interface QuestionFilterParams {
+  exam_id?: string;
+  subject?: string;
+  topic?: string;
+  level?: string;
+  question_type?: string;
+  content?: string;
+  question_number?: number;
+  score_weight?: number;
+}
+
+export interface GetQuestionsParams extends QuestionFilterParams {
+  page?: number;
+  page_size?: number;
+}
